dad5d3188d5812ba803c0d695fba34730fb17283
Kent clarence Evangelista <kentclarence5368@gmail.com>
2025-11-29 01:25:13 +0800

    fixing some errors for prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if ($this->app->environment('production')) {
-        URL::forceScheme('https');
-    }
     }
 }
